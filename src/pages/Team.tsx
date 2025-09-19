794885e3f23573c48a2d0b3779cc9e47ecc3898d
Priyansh2116 <priyanshsonthalia23@gmail.com>
2025-09-19 20:44:50 +0530

    abhinav image part2

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -50,7 +50,7 @@ const Team = () => {
   const interns = [
     { name: 'Lakshay Chhabra', title: 'Intern', image: '', linkedin: '#', bio: '' },
     { name: 'Priyansh Sonthalia', title: 'Intern', image: '', linkedin: '#', bio: '' },
-    { name: 'Abhinav', title: 'Intern', image: '/abhinav.jpg', linkedin: '#', bio: '' }
+    { name: 'Abhinav', title: 'Intern', image: 'https://priyanshsonthalia23-nmbuw.wordpress.com/wp-content/uploads/2025/09/whatsapp-image-2025-09-19-at-19.25.51-min-2.jpeg', linkedin: '#', bio: '' }
   ];
 
   const TeamCard = ({ member, showEmail = false }) => (
